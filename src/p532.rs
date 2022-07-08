7e17da1e6de28e94cbfb920a726636df045bbb72
George Sanger <paperclipstudio@gmail.com>
2022-07-08 09:16:35 +0000

    removed commented out code in p532

--- src/p532.rs
+++ src/p532.rs
@@ -1,28 +1,13 @@
 #![allow(dead_code)]
 
 use std::collections::HashSet;
-use std::collections::HashMap;
 
 struct Solution {}
 
 impl Solution {
     pub fn find_pairs(nums: Vec<i32>, k:i32) -> i32 {
-    //     let mut count = HashMap::new();
         let mut result = 0;
-
-    //     for num in  nums {
-    //         count.insert(num, count.get(&num).unwrap_or(&0) + 1);
-    //     }
-    //     for num in count.keys() {
-    //         result += num * count.get(&(num+k)).unwrap_or(&0);
-    //     }
-    //     return result;
-    // }
-
-
         let mut set = HashSet::new();
-
-        
 
         if k == 0 {
             let mut set_double = HashSet::new();
@@ -51,7 +36,6 @@ mod tests {
     #[test]
     fn basic() {
         assert_eq!(Solution::find_pairs(vec!{3,1,4,1,5}, 2), 2);
-        // 1 1 3 4 5
     }
 
 
@@ -64,10 +48,4 @@ mod tests {
     fn k_is_zerp() {
         assert_eq!(Solution::find_pairs(vec![1,3,1,5,4], 0), 1);
     }
-
-    // #[test]
-    // fn many_to_many() {
-    //     assert_eq!(Solution::find_pairs(vec![1,1,3,3,3], 2), 6);
-    // }
-
 }
